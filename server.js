fe13a178aa67b899c298295ec9535d08e8522542
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-10-24 14:34:52 -0400

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -8,7 +9,7 @@ app.use(express.static('public'));
 const bookmarkController = require('./controllers/bookmarks.js');
 app.use('/bookmarks', bookmarkController);
 
-app.listen(PORT || 3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('listening...');
 });
 
